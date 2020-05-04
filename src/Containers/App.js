a1e2a68cec36ff529eb618452ef105386450de20
Artyom Okun <artyom@okun.it>
2020-05-04 06:17:34 +0300

    Finish ErrorBoundry

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
 import './App.css'
+import ErrorBoundry from "../Components/ErrorBoundry";
 
 class App extends Component {
 
@@ -37,7 +38,9 @@ class App extends Component {
                 <h1 className='f2'>Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filtered}/>
+                    <ErrorBoundry>
+                        <CardList robots={filtered}/>
+                    </ErrorBoundry>
                 </Scroll>
             </div>);
     }
